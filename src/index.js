b193138cb77450fcb9603973e430d668982be9f5
Luis Carlos Enríquez <lcenriquez@users.noreply.github.com>
2020-05-09 09:45:12 -0500

    Remove unused config file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App/index';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
-import config from './config';
 
 const store = createStore(reducer);
 
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            {/* basename="/datta-able" */}
-            <App />
-        </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* basename="/datta-able" */}
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
